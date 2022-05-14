064093e778ee6b5a515303f03c1f2d6861aa570e
Josh Klein <kleinjjosh74@gmail.com>
2022-05-14 17:51:04 -0400

    reconfigured file structure based on MVC standards/ set up handlebars/ created homepage

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Imports handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+//Sets up handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Turn on routes
 app.use(routes);
+
 
 //Turn on connection to db and server
 sequelize.sync({ force:false }).then(() => {
